33442a1336a2b0a25cebb3f84def1cda2e8272b7
taher-dev <monmoyzx@gmail.com>
2026-06-30 14:45:19 +0600

    Update project image paths to use imported assets for consistency

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,51 +1,59 @@
+import project1 from "../assets/images/projects/project-1.jpg";
+import project2 from "../assets/images/projects/project-2.jpg";
+import project3 from "../assets/images/projects/project-3.jpg";
+import project4 from "../assets/images/projects/project-4.jpg";
+import project5 from "../assets/images/projects/project-5.jpg";
+import project6 from "../assets/images/projects/project-6.jpg";
+import project7 from "../assets/images/projects/project-7.jpg";
+
 export const projects = [
   {
     id: 1,
     title: "Sundown Studio",
     category: "web",
-    image: "src/assets/images/projects/project-1.jpg",
+    image: project1,
     link: "https://github.com/taher-dev/Sundown-studio-clone",
   },
   {
     id: 2,
     title: "Nike - Product Landing Page",
     category: "web",
-    image: "src/assets/images/projects/project-2.jpg",
+    image: project2,
     link: "https://github.com/taher-dev/nike-web-programming-project",
   },
   {
     id: 3,
     title: "Virtual Dice Roller",
     category: "web",
-    image: "src/assets/images/projects/project-3.jpg",
+    image: project3,
     link: "https://github.com/taher-dev/roll-dice",
   },
   {
     id: 4,
     title: "Employee Management System",
     category: "software",
-    image: "src/assets/images/projects/project-4.jpg",
+    image: project4,
     link: "https://github.com/taher-dev/Employee-Management-System",
   },
   {
     id: 5,
     title: "Tic Tac Toe",
     category: "web",
-    image: "src/assets/images/projects/project-5.jpg",
+    image: project5,
     link: "https://github.com/taher-dev/tic-tac-toe",
   },
   {
     id: 6,
     title: "Student Management System",
     category: "cli",
-    image: "src/assets/images/projects/project-6.jpg",
+    image: project6,
     link: "https://github.com/taher-dev/student-management-system-C",
   },
   {
     id: 7,
     title: "3D Restaurant Model",
     category: "others",
-    image: "src/assets/images/projects/project-7.jpg",
+    image: project7,
     link: "https://github.com/taher-dev/3D-restaurant-model-AutoCAD",
   },
 ];
